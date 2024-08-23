a01072490ba07017fdae05795397218067a2d5c6
onfekioncrl30 <gnarnd4531@gmail.com>
2024-08-23 14:56:04 -0300

    small fix

--- src/main/java/net/mcreator/modphoton/procedures/ArcanetableanimationProcedure.java
+++ src/main/java/net/mcreator/modphoton/procedures/ArcanetableanimationProcedure.java
@@ -1,6 +1,13 @@
 package net.mcreator.modphoton.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.core.BlockPos;
 
 public class ArcanetableanimationProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
@@ -8,7 +15,7 @@ public class ArcanetableanimationProcedure {
 		double sx = 0;
 		double sy = 0;
 		double sz = 0;
-		if (!world.getEntitiesOfClass(Player.class, AABB.ofSize(new Vec3(x, y, z), 8, 8, 8), e -> true).isEmpty()) {
+		if (!world.getEntitiesOfClass(Player.class, AABB.ofSize(new Vec3(x, y, z), 6, 6, 6), e -> true).isEmpty()) {
 			{
 				int _value = 1;
 				BlockPos _pos = BlockPos.containing(x, y, z);
@@ -16,7 +23,7 @@ public class ArcanetableanimationProcedure {
 				if (_bs.getBlock().getStateDefinition().getProperty("animation") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
 					world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
 			}
-		} else if (!world.getEntitiesOfClass(ServerPlayer.class, AABB.ofSize(new Vec3(x, y, z), 8, 8, 8), e -> true).isEmpty()) {
+		} else if (!world.getEntitiesOfClass(ServerPlayer.class, AABB.ofSize(new Vec3(x, y, z), 6, 6, 6), e -> true).isEmpty()) {
 			{
 				int _value = 1;
 				BlockPos _pos = BlockPos.containing(x, y, z);
